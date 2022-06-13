0f2523ea87186e062aef5aa1ba5b31cbda5397d2
Fred230 <frednyambok2030@gmail.com>
2022-06-13 15:36:09 +0300

    Initial commit

---
+++ app/src/main/java/com/fredrick/bmi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fredrick.bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
